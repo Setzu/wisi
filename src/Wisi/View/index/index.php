3a8e99e4698dc9498f5e1413f7e5febc9874b23b
gfp <david.blanchard@gfpfrance.com>
2018-03-21 17:33:33 +0100

    Modification des bouttons

--- src/Wisi/View/index/index.php
+++ src/Wisi/View/index/index.php
@@ -35,10 +35,14 @@
                 <br>
                 <div class="col-md-3"></div>
                 <div class="col-md-3">
-                    <a href="/job" class="btn btn-default btn-block btn-lg">Suivre un job</a>
+                    <a href="/job" class="btn btn-success btn-block btn-lg">
+                        <span class="glyphicon glyphicon-eye-open">&nbsp;</span>Suivre un job
+                    </a>
                 </div>
                 <div class="col-md-3">
-                    <a href="/job/display" class="btn btn-default btn-block btn-lg">Affichage des jobs</a>
+                    <a href="/job/display" class="btn btn-success btn-block btn-lg">
+                        <span class="glyphicon glyphicon-th-list">&nbsp;</span>Affichage des jobs
+                    </a>
                 </div>
             </div>
             <br>
@@ -47,10 +51,14 @@
                 <br>
                 <div class="col-md-3"></div>
                 <div class="col-md-3">
-                    <a href="/system" class="btn btn-default btn-block btn-lg">Ajouter un système</a>
+                    <a href="/system" class="btn btn-success btn-block btn-lg">
+                        <span class="glyphicon glyphicon-plus">&nbsp;</span>Ajouter un système
+                    </a>
                 </div>
                 <div class="col-md-3">
-                    <a href="/system/update" class="btn btn-default btn-block btn-lg">Modifier un système</a>
+                    <a href="/system/update" class="btn btn-success btn-block btn-lg">
+                        <span class="glyphicon glyphicon-pencil">&nbsp;</span>Modifier un système
+                    </a>
                 </div>
             </div>
             <br>
@@ -59,7 +67,9 @@
                 <br>
                 <div class="col-md-3"></div>
                 <div class="col-md-3">
-                    <a href="/index/timer" class="btn btn-default btn-block btn-lg">Modifier le timer de rafraichissement</a>
+                    <a href="/index/timer" class="btn btn-success btn-block btn-lg">
+                        <span class="glyphicon glyphicon-time">&nbsp;</span>Modifier le cycle d'actualisation
+                    </a>
                 </div>
             </div>
         </div>
